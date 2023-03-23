dd9c3fc1e6e7244cbf914304b461b1950b958e87
Alicia <Alicia@gmail.com>
2023-03-23 15:56:55 -0500

    Add plugin and dependencies

--- app/src/main/java/com/bignerdranch/android/criminalIntent_1/Crime.kt
+++ app/src/main/java/com/bignerdranch/android/criminalIntent_1/Crime.kt
@@ -4,7 +4,8 @@ import java.util.*
 
 class Crime {
 
-    data class Crime(val id: UUID = UUID.randomUUID(),
+    @Entity
+    data class Crime(@PrimaryKey val id: UUID = UUID.randomUUID(),
                      var title: String = "",
                      var date: Date = Date(),
                      var isSolved: Boolean = false)
