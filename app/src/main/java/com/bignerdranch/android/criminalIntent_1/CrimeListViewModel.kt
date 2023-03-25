3352e76a7ba5f1704e24aca21c3151e2c24127fb
Alicia <Alicia@gmail.com>
2023-03-24 23:47:12 -0500

    Add LiveData object

--- app/src/main/java/com/bignerdranch/android/criminalIntent_1/CrimeListViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/criminalIntent_1/CrimeListViewModel.kt
@@ -1,16 +1,12 @@
 package com.bignerdranch.android.criminalIntent_1
 
 import androidx.lifecycle.ViewModel
+import database.CrimeRepository
+
 
 class CrimeListViewModel : ViewModel() {
 
-    val crimes = mutableListOf<Crime>()
-    init {
-        for (i in 0 until 100) {
-            val crime = Crime()
-            crime.title = "Crime #$i="
-            crime.isSolved = i % 2 == 0
-            crimes += crime
-        }
-    }
+
+    private val crimeRepository = CrimeRepository.get()
+    val crimeListLiveData = crimeRepository.getCrimes()
 }
